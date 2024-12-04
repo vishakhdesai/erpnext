3620ddab3457398880e80e783c2ff8313ba2ccad
mahsem <137205921+mahsem@users.noreply.github.com>
2024-12-04 08:58:16 +0100

    fix: add labels for translation in sales_order.js
    (cherry picked from commit d544328ffe629ed1dfb22a593db4044de8e30131)

--- erpnext/selling/doctype/sales_order/sales_order.js
+++ erpnext/selling/doctype/sales_order/sales_order.js
@@ -68,7 +68,7 @@ frappe.ui.form.on("Sales Order", {
 				target: frm,
 				setters: [
 					{
-						label: 'Supplier',
+						label: __('Supplier'),
 						fieldname: 'supplier',
 						fieldtype: 'Link',
 						options: 'Supplier'
@@ -376,7 +376,7 @@ erpnext.selling.SalesOrderController = class SalesOrderController extends erpnex
 						target: me.frm,
 						setters: [
 							{
-								label: "Customer",
+								label: __("Customer"),
 								fieldname: "party_name",
 								fieldtype: "Link",
 								options: "Customer",
@@ -430,7 +430,7 @@ erpnext.selling.SalesOrderController = class SalesOrderController extends erpnex
 				}
 				else {
 					const fields = [{
-						label: 'Items',
+						label: __('Items'),
 						fieldtype: 'Table',
 						fieldname: 'items',
 						description: __('Select BOM and Qty for Production'),
@@ -724,7 +724,7 @@ erpnext.selling.SalesOrderController = class SalesOrderController extends erpnex
 					"default": 0
 				},
 				{
-					fieldname: 'items_for_po', fieldtype: 'Table', label: 'Select Items',
+					fieldname: 'items_for_po', fieldtype: 'Table', label: __('Select Items'),
 					fields: [
 						{
 							fieldtype:'Data',
